23c811e2b13e8228dd3fa4b093c119ea737f2917
Mahialam Rahat <mahialam408@gmail.com>
2026-08-13 19:50:34 +0600

    Refactor authentication flow to include refresh token verification and access token generation in getCurrentUser function

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,17 +1,49 @@
 import { cookies } from "next/headers";
-import { verifyAccessToken } from "./jwt";
+import {
+  generateAccessToken,
+  verifyAccessToken,
+  verifyRefreshToken,
+} from "./jwt";
 
 export async function getCurrentUser() {
-    const cookieStore = await cookies();
-    const accessToken = cookieStore.get("accessToken")?.value;
-    if (!accessToken) {
-        return null
-    }
+  const cookieStore = await cookies();
 
+  const accessToken = cookieStore.get("accessToken")?.value;
+  const refreshToken = cookieStore.get("refreshToken")?.value;
+
+  // কোনো access token নেই
+  if (accessToken) {
+    // Access token verify
     const decoded = verifyAccessToken(accessToken);
-    if (!decoded) { 
-        return null
-    }
 
-    return decoded;
+    // Access token valid
+    if (decoded) {
+      return decoded;
+    }
+  }
+
+  // Access token expired/invalid
+  if (!refreshToken) {
+    return null;
+  }
+
+  // Refresh token verify
+  const decodedRefresh = verifyRefreshToken(refreshToken);
+
+  if (!decodedRefresh) {
+    return null;
+  }
+
+  // নতুন access token generate
+  await generateAccessToken({
+    userId: decodedRefresh.userId,
+    email: decodedRefresh.email,
+    role: decodedRefresh.role,
+  });
+
+  return {
+    userId: decodedRefresh.userId,
+    email: decodedRefresh.email,
+    role: decodedRefresh.role,
+  };
 }
--- src/proxy.ts
+++ src/proxy.ts
@@ -1,17 +1,27 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
+import { getCurrentUser } from './lib/auth';
  
 // This function can be marked `async` if using `await` inside
-export function proxy(request: NextRequest) {
+export async function proxy(request: NextRequest) {
+
+  const user = await getCurrentUser();
+  console.log(user, 'user')
+  if (!user) { 
+    return NextResponse.redirect(
+      new URL("/login", request.url)
+    );
+  }
+  
 
   console.log(request)
 
-  return NextResponse.redirect(new URL('/home', request.url))
+  return NextResponse.next()
 }
  
 // Alternatively, you can use a default export:
 // export default function proxy(request: NextRequest) { ... }
  
 export const config = {
-  matcher: '/about/:path*',
+  matcher: '/',
 }
