6056a455dd4f29816dbbec764905fda17639f60b
Mahialam Rahat <mahialam408@gmail.com>
2026-08-19 21:21:33 +0600

    fix

---
+++ src/app/dashboard/profile/page.tsx
@@ -0,0 +1,13 @@
+import DeviceInfo from '@/app/test2/page';
+import React from 'react';
+
+const ProfilePage = () => {
+    return (
+        <div>
+            This is profile page
+            <DeviceInfo />
+        </div>
+    );
+};
+
+export default ProfilePage;
